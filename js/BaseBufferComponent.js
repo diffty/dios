4f06011029c72128286a845452f84296df1f9717
DiFFtY <diffty@gmail.com>
2020-11-15 13:31:12 +0100

    BaseBufferComponent: added next transition feature to schedule a precise transition for the next component switch
    Changed setRawBuffer method to setBuffer

--- js/BaseBufferComponent.js
+++ js/BaseBufferComponent.js
@@ -1,11 +1,14 @@
+//const { default: tickerSystemInstance } = require("..");
+
 class BaseBufferComponent {
     constructor() {
         this.rawBuffer = "";
         this.buffer = "";
         this.effects = [];
+        this.nextTransition = null;
     }
 
-    setRawBuffer(newBuffer) {
+    setBuffer(newBuffer) {
         this.rawBuffer = newBuffer;
         this.buffer = newBuffer;
     }
@@ -19,18 +22,29 @@ class BaseBufferComponent {
     }
 
     applyEffects() {
-        for (var i in this.effects) {
+        for (let i in this.effects) {
             this.buffer = this.effects[i].applyEffect(this.buffer);
         }
     }
+
+    setNextTransition(nextTransition) {
+        this.nextTransition = nextTransition;
+        this.nextTransition.setIsOneShot(true);
+    }
     
     update(deltaTime) {
-        for (var i in this.effects) {
+        for (let i in this.effects) {
             this.effects[i].update(deltaTime);
         }
     }
 
     draw(deltaTime) {
         
+    }
+
+    onActive() {
+        if (this.nextTransition) {
+            tickerSystemInstance.transitionsStack.unshift(this.nextTransition);
+        }
     }
 }
--- js/TextBufferComponent.js
+++ js/TextBufferComponent.js
@@ -8,7 +8,7 @@ class TextBufferComponent extends BaseBufferComponent {
     }
 
     setText(text) {
-        this.setRawBuffer(text);
+        this.setBuffer(text);
     }
 
     draw(deltaTime) {
