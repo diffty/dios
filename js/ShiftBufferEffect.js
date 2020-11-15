4da50ed8131a7d7b9468fce0afe19c3815a88f51
DiFFtY <diffty@gmail.com>
2020-11-15 13:40:34 +0100

    ShiftBufferEffect: changed mechanic to use StepAnimable

--- js/ShiftBufferEffect.js
+++ js/ShiftBufferEffect.js
@@ -1,12 +1,23 @@
-class ShiftBufferEffect extends Animable(BaseBufferEffect) {
+class ShiftBufferEffect extends StepAnimable(BaseBufferEffect) {
     constructor() {
         super();
+        this.stepNum = 0;
     }
 
     applyEffect(buffer) {
-        let currShift = Math.floor(this.animProgress * buffer.length);
+        if (buffer.length > 0)
+            this.stepNum = this.stepNum % buffer.length;
+        else
+            this.stepNum = 0;
+
+        let currShift = Math.floor(this.stepNum);
         let bufferArray = buffer.split("");
         let shiftedChar = bufferArray.splice(0, currShift);
         return bufferArray.join("") + shiftedChar.join("");
+    }
+
+    onStepEnd() {
+        super.onStepEnd();
+        this.stepNum++;
     }
 }
