6b12a7d6c90438ea2eb687661a07ae14674f853a
DiFFtY <diffty@gmail.com>
2020-11-15 13:42:11 +0100

    New components to display Twitch infos

---
+++ js/TwitchViewersComponent.js
@@ -0,0 +1,30 @@
+class TwitchViewersComponent extends BaseBufferComponent {
+    constructor(userId, clientId) {
+        super();
+
+        this.userId = userId;
+        this.clientId = clientId;
+
+        this.twitchInterface = new TwitchInterface(clientId);
+
+        this.updateViewerCount();
+    }
+
+    updateViewerCount() {
+        this.twitchInterface.getViewersCount(
+            this.userId,
+            (streamsList) => {
+                let viewersCount = 0;
+                if (streamsList.length > 0) {
+                    viewersCount = streamsList[0].viewer_count;
+                }
+                this.setBuffer("Viewers Count: " + viewersCount.toString())
+            }
+        )
+    }
+
+    draw(deltaTime) {
+        this.buffer = this.rawBuffer;
+        //this.applyEffects();
+    }
+}
