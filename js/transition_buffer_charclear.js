1b8c1974245a28f158053edd76a8f5ef5d15294e
DiFFtY <diffty@gmail.com>
2020-11-19 11:21:46 +0100

    CharClear transition: changed clear character from # to *

--- js/transition_buffer_charclear.js
+++ js/transition_buffer_charclear.js
@@ -17,10 +17,10 @@ class CharClearTransitionComponent extends BaseTransitionComponent {
         let hiddenCharNum = maxBufferSize * v;
         
         if (this.animProgress <= 0.5) {
-            this.buffer = ['#'.repeat(hiddenCharNum), bufferA.slice(hiddenCharNum, Math.max(0, bufferA.length))].join('');
+            this.buffer = ['*'.repeat(hiddenCharNum), bufferA.slice(hiddenCharNum, Math.max(0, bufferA.length))].join('');
         }
         else {
-            this.buffer = [bufferB.slice(0, Math.max(0, bufferB.length-hiddenCharNum+1)), '#'.repeat(hiddenCharNum)].join('');
+            this.buffer = [bufferB.slice(0, Math.max(0, bufferB.length-hiddenCharNum+1)), '*'.repeat(hiddenCharNum)].join('');
         }
     }
 }
